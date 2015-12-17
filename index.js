475773497d61b26aed9576adb38ad2e884bd2cda
Soichiro-Yoshimura <soichiro_yoshimura@dwango.co.jp>
2015-12-17 15:05:00 +0900

    isDone 関数に対応

--- index.js
+++ index.js
@@ -31,12 +31,30 @@
 	}
 	
 	/**
+	 * タスクと完了したかどうかが含まれる配列を受け取り、完了したかを返す
+	 * @param {array} taskAndIsDonePair
+	 * @return {boolean} 完了したかどうか
+	 */
+	function isDone(taskAndIsDonePair) {
+		return taskAndIsDonePair[1];
+	}
+	
+	/**
+	 * タスクと完了したかどうかが含まれる配列を受け取り、完了していないかを返す
+	 * @param {array} taskAndIsDonePair
+	 * @return {boolean} 完了していないかどうか
+	 */
+	function isNotDone(taskAndIsDonePair) {
+		return !isDone(taskAndIsDonePair);
+	}
+	
+	/**
 	 * TODOの一覧の配列を取得する
 	 * @return {array}
 	 */
 	function list() {
 		return Array.from(tasks)
-			.filter(t => !t[1])
+			.filter(isNotDone)
 			.map(t => t[0]);
 	}
 	
@@ -57,7 +75,7 @@
 	 */
 	function donelist() {
 		return Array.from(tasks)
-			.filter(t => t[1])
+			.filter(isDone)
 			.map(t => t[0]);
 	}
 	
