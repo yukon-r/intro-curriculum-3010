a91eb688b9dc8ac8fc4c3f79a9f9b780f0cd999e
DDRAGON <davide.0829@gmail.com>
2016-09-28 16:17:35 +0900

    即時実行関数の削除

--- test.js
+++ test.js
@@ -1,23 +1,21 @@
-(function () {
-	'use strict';
-	const todo = require('./index.js');
-	const assert = require('assert');
-	
-	// todo と list のテスト
-	todo.todo('ノートを買う');
-	todo.todo('鉛筆を買う');
-	assert.deepEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
-	
-	// done と donelist のテスト
-	todo.done('鉛筆を買う');
-	assert.deepEqual(todo.list(), ['ノートを買う']);
-	assert.deepEqual(todo.donelist(), ['鉛筆を買う']);
-	
-	// del のテスト
-	todo.del('ノートを買う');
-	todo.del('鉛筆を買う');
-	assert.deepEqual(todo.list(), []);
-	assert.deepEqual(todo.donelist(), []);
+'use strict';
+const todo = require('./index.js');
+const assert = require('assert');
 
-	console.log('テストが正常に完了しました');
-})();
+// todo と list のテスト
+todo.todo('ノートを買う');
+todo.todo('鉛筆を買う');
+assert.deepEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
+
+// done と donelist のテスト
+todo.done('鉛筆を買う');
+assert.deepEqual(todo.list(), ['ノートを買う']);
+assert.deepEqual(todo.donelist(), ['鉛筆を買う']);
+
+// del のテスト
+todo.del('ノートを買う');
+todo.del('鉛筆を買う');
+assert.deepEqual(todo.list(), []);
+assert.deepEqual(todo.donelist(), []);
+
+console.log('テストが正常に完了しました');
