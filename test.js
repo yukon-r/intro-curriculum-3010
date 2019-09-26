c9180c3f911907a8caab086664422d980f517fcc
yukon-r <teacup6955@gmail.com>
2019-09-26 10:29:45 +0900

    tasks.json削除後のテスト実行に変更

--- test.js
+++ test.js
@@ -1,21 +1,26 @@
 'use strict';
-const todo = require('./index.js');
 const assert = require('assert');
 
-// todo と list のテスト
-todo.todo('ノートを買う');
-todo.todo('鉛筆を買う');
-assert.deepEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
+// テストの前に永続化されているファイルを消す
+const fs = require('fs');
+fs.unlink('./tasks.json',(err) => {
+    const todo = require('./index.js');
 
-// done と donelist のテスト
-todo.done('鉛筆を買う');
-assert.deepEqual(todo.list(), ['ノートを買う']);
-assert.deepEqual(todo.donelist(), ['鉛筆を買う']);
+    // todo と list のテスト
+    todo.todo('ノートを買う');
+    todo.todo('鉛筆を買う');
+    assert.deepEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
 
-// del のテスト
-todo.del('ノートを買う');
-todo.del('鉛筆を買う');
-assert.deepEqual(todo.list(), []);
-assert.deepEqual(todo.donelist(), []);
+    // done と donelist のテスト
+    todo.done('鉛筆を買う');
+    assert.deepEqual(todo.list(), ['ノートを買う']);
+    assert.deepEqual(todo.donelist(), ['鉛筆を買う']);
 
-console.log('テストが正常に完了しました');
+    // del のテスト
+    todo.del('ノートを買う');
+    todo.del('鉛筆を買う');
+    assert.deepEqual(todo.list(), []);
+    assert.deepEqual(todo.donelist(), []);
+
+    console.log('テストが正常に完了しました');
+});
